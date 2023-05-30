408fcf0be3ddc099ce19f0c4d3b9c6ad0a1e7120
fulinpeng <37953041+fulinpeng@users.noreply.github.com>
2023-05-30 16:08:51 +0800

    Create 5_ternary.sol
    ternary

---
+++ 5_ternary.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract EtherUnits {
+    function foo(uint x) public pure returns (uint) {
+        if (x < 10) {
+            return 0;
+        } else if (x < 20) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+    function ternary(uint _x) public pure returns (uint) {
+        // if (_x < 10) {
+        //     return 1;
+        // }
+        // return 2;
+
+        // shorthand way to write if / else statement
+        return _x < 10 ? 1 : 2;
+    }
+
+    function loop() public pure {
+        // for loop
+        for (uint i = 0; i < 10; i++) {
+            if (i == 3) {
+                // Skip to next iteration with continue
+                continue;
+            }
+            if (i == 5) {
+                // Exit loop with break
+                break;
+            }
+        }
+
+        // while loop
+        uint j;
+        while (j < 10) {
+            j++;
+        }
+    }
+}
